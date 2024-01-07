b9fce14dd3f820a8c539688694078b7f2d883961
Federico Guerinoni <guerinoni.federico@gmail.com>
2024-01-07 17:17:22 +0100

    Add filter for read/unread
    Signed-off-by: Federico Guerinoni <guerinoni.federico@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -31,6 +31,9 @@ struct SubjectItem {
     type_: String,
 }
 
+static FETCH_ALL_NOTIFICATIONS: std::sync::atomic::AtomicBool =
+    std::sync::atomic::AtomicBool::new(false);
+
 fn main() {
     let home = home::home_dir();
     let hosts_path = home.unwrap().join(".config/gh/hosts.yml");
@@ -74,6 +77,36 @@ fn main() {
         }
     });
 
+    main_window.on_mark_done({
+        move |id| {
+            let id = String::from(id);
+            tokio::runtime::Runtime::new()
+                .unwrap()
+                .block_on(mark_thread_done(id))
+        }
+    });
+
+    main_window.on_apply_filter({
+        let mw = main_window.as_weak();
+        move || {
+            let unread_only = mw.unwrap().get_unread_only();
+            println!("apply filter: {}", unread_only);
+            FETCH_ALL_NOTIFICATIONS.store(!unread_only, std::sync::atomic::Ordering::Relaxed);
+
+            std::thread::spawn({
+                let mw = mw.clone();
+                move || {
+                    tokio::runtime::Runtime::new()
+                        .unwrap()
+                        .block_on(update_model(
+                            mw.clone(),
+                            &std::env::var("GITHUB_TOKEN").unwrap(),
+                        ))
+                }
+            });
+        }
+    });
+
     main_window.run().unwrap();
 }
 
@@ -93,8 +126,87 @@ async fn mark_thread_read(id: String) {
         .await
         .unwrap();
 
+    if response.status().is_success() {
+        println!("mark read success");
+    } else {
+        println!("mark read failed");
+    }
+
     let r = response.text().await.unwrap();
     println!("{}", r);
+}
+
+async fn mark_thread_done(id: String) {
+    println!("mark done: {}", id);
+    let url = format!("https://api.github.com/notifications/threads/{}", id);
+    let token = std::env::var("GITHUB_TOKEN").unwrap();
+
+    let client = reqwest::Client::new();
+    let response = client
+        .delete(url)
+        .header("Accept", "application/vnd.github+json")
+        .header("Authorization", format!("Bearer {}", token))
+        .header("X-GitHub-Api-Version", "2022-11-28")
+        .header("User-Agent", "ghn")
+        .send()
+        .await
+        .unwrap();
+
+    if response.status().is_success() {
+        println!("mark done success");
+    } else {
+        println!("mark done failed");
+    }
+
+    let r = response.text().await.unwrap();
+    println!("{}", r);
+}
+
+async fn fetch_notifications(token: &str, all: bool) -> Vec<Notification> {
+    let url = "https://api.github.com/notifications";
+    let client = reqwest::Client::new();
+    let query = vec![("all", all.to_string())];
+    let response = client
+        .get(url)
+        .query(&query)
+        .header("Accept", "application/vnd.github+json")
+        .header("Authorization", format!("Bearer {}", token))
+        .header("X-GitHub-Api-Version", "2022-11-28")
+        .header("User-Agent", "ghn")
+        .send()
+        .await
+        .unwrap();
+
+    let r = response.text().await.unwrap();
+    println!("{}", r);
+    let notifications: Vec<NotificationItem> = serde_json::from_str(&r).unwrap();
+
+    notifications
+        .iter()
+        .map(|item| Notification {
+            id: item.id.clone().into(),
+            unread: item.unread,
+            reason: item.reason.clone().into(),
+            url: item.url.clone().into(),
+            subject: Subject {
+                title: item.subject.title.clone().into(),
+                url: item.subject.url.clone().unwrap_or_default().into(),
+                latest_comment_url: item
+                    .subject
+                    .latest_comment_url
+                    .clone()
+                    .unwrap_or("".into())
+                    .into(),
+                type_: item.subject.type_.clone().into(),
+            },
+            repository: Repository {
+                name: item.repository.name.clone().into(),
+                full_name: item.repository.full_name.clone().into(),
+                url: item.repository.url.clone().into(),
+                html_url: item.repository.html_url.clone().into(),
+            },
+        })
+        .collect::<Vec<Notification>>()
 }
 
 async fn worker_fetch(mw: slint::Weak<MainWindow>) {
@@ -104,54 +216,20 @@ async fn worker_fetch(mw: slint::Weak<MainWindow>) {
     loop {
         interval.tick().await;
 
-        let url = "https://api.github.com/notifications";
-        let client = reqwest::Client::new();
-        let response = client
-            .get(url)
-            .header("Accept", "application/vnd.github+json")
-            .header("Authorization", format!("Bearer {}", token))
-            .header("X-GitHub-Api-Version", "2022-11-28")
-            .header("User-Agent", "ghn")
-            .send()
-            .await
-            .unwrap();
-
-        let r = response.text().await.unwrap();
-        println!("{}", r);
-        let notifications: Vec<NotificationItem> = serde_json::from_str(&r).unwrap();
-
-        let notifications = notifications
-            .iter()
-            .map(|item| Notification {
-                id: item.id.clone().into(),
-                unread: item.unread,
-                reason: item.reason.clone().into(),
-                url: item.url.clone().into(),
-                subject: Subject {
-                    title: item.subject.title.clone().into(),
-                    url: item.subject.url.clone().unwrap_or("".into()).into(),
-                    latest_comment_url: item
-                        .subject
-                        .latest_comment_url
-                        .clone()
-                        .unwrap_or("".into())
-                        .into(),
-                    type_: item.subject.type_.clone().into(),
-                },
-                repository: Repository {
-                    name: item.repository.name.clone().into(),
-                    full_name: item.repository.full_name.clone().into(),
-                    url: item.repository.url.clone().into(),
-                    html_url: item.repository.html_url.clone().into(),
-                },
-            })
-            .collect::<Vec<Notification>>();
-
-        mw.clone()
-            .upgrade_in_event_loop(move |h| {
-                let model = Rc::new(slint::VecModel::<Notification>::from(notifications));
-                h.set_notifications_model(model.into());
-            })
-            .unwrap();
+        update_model(mw.clone(), &token).await;
     }
+}
+
+async fn update_model(mw: slint::Weak<MainWindow>, token: &str) {
+    let notifications = fetch_notifications(
+        token,
+        FETCH_ALL_NOTIFICATIONS.load(std::sync::atomic::Ordering::Relaxed),
+    )
+    .await;
+    mw.clone()
+        .upgrade_in_event_loop(move |h| {
+            let model = Rc::new(slint::VecModel::<Notification>::from(notifications));
+            h.set_notifications_model(model.into());
+        })
+        .unwrap();
 }
